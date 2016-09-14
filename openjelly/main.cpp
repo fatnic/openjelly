c61f0ad0b64cf2022213c8ce68eac23620d64dd8
Dave Nicholson <fatnic388@gmail.com>
2016-09-14 22:46:54 +0100

    Uncommented to texture that I needed

--- openjelly/main.cpp
+++ openjelly/main.cpp
@@ -105,7 +105,7 @@ int main() {
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tW, tH, 0, GL_RGB, GL_UNSIGNED_BYTE, texImage);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tW, tH, 0, GL_RGB, GL_UNSIGNED_BYTE, texImage);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	stbi_image_free(texImage);
 

28aa932184904cea850e5f19c9d841b4d8edec59
Dave Nicholson <fatnic388@gmail.com>
2016-09-17 11:48:41 +0100

    multipple boxes added

--- openjelly/main.cpp
+++ openjelly/main.cpp
@@ -67,6 +67,19 @@ int main() {
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	Shader boxShader  ("shaders/basic.vert", "shaders/basic.frag");
 	Shader lightShader("shaders/light.vert", "shaders/light.frag");
 
@@ -120,19 +133,31 @@ int main() {
 		view = camera.getView();
 		projection = glm::perspective(camera.fov, (GLfloat)(window.getWidth() / window.getHeight()), 0.1f, 100.0f);
 
-		boxShader.setUniform("model", &model);
 		boxShader.setUniform("view", &view);
 		boxShader.setUniform("projection", &projection);
+
+		for (GLuint i = 0; i < 10; i++)
+		{
+			model = glm::mat4();
+			model = glm::translate(model, cubePositions[i]);
+			GLfloat angle = 20.0f * i;
+			model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+			boxShader.setUniform("model", &model);
+			container.draw(36);
+		}
+
 
 		model = glm::mat4();
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.1f));
 
-		lightShader.setUniform("model", &model);
 		lightShader.setUniform("view", &view);
 		lightShader.setUniform("projection", &projection);
+		lightShader.setUniform("model", &model);
+
+
+
 		
-		container.draw(36);
 		light.draw(36);
 		
 		window.update();

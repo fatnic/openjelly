22074457fe47892bcc32dcf684c5d9b73b2f23c4
Dave Nicholson <fatnic388@gmail.com>
2016-09-18 11:34:48 +0100

    some crazy freetype font rendering

--- openjelly/main.cpp
+++ openjelly/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <map>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #include "window.h"
 #include "shader.h"
@@ -16,6 +20,18 @@ GLfloat lastY = 300;
 bool firstMouseMove = true;
 
 void processMovement(Camera* camera, Window* window);
+void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 colour);
+
+struct Character {
+	GLuint TextureID;
+	glm::ivec2 size;
+	glm::ivec2 bearing;
+	GLuint advance;
+};
+
+std::map<GLchar, Character> characters;
+
+GLuint vao, vbo;
 
 int main() {
 
@@ -24,6 +40,7 @@ int main() {
 	Camera camera(glm::vec3(-1.0f, 1.0f, 6.0f));
 
 	Shader basicShader("shaders/basic.vert", "shaders/basic.frag");
+	Shader textShader("shaders/text.vert", "shaders/text.frag");
 
 	ObjLoader mesh("objects/box.obj");
 
@@ -44,7 +61,78 @@ int main() {
 	basicShader.setUniform("projection", &projection);
 
 	Texture meshTexture("objects/dice.png");
-	glBindTexture(GL_TEXTURE_2D, meshTexture.id);
+
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE:: Could not initialize freetype" << std::endl;
+
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/monaco.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE:: Failed to load font" << std::endl;
+
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// Load character glyph 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// Set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// Now store character for later use
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glm::mat4 textProjection = glm::ortho(0.0f, 600.0f, 0.0f, 600.0f);
+	textShader.setUniform("projection", &textProjection);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	while (!window.closed()) {
 
@@ -58,7 +146,10 @@ int main() {
 		view = camera.getView();
 		basicShader.setUniform("view", &view);
 
+		glBindTexture(GL_TEXTURE_2D, meshTexture.id);
 		obj.draw(mesh.data.size() / 8);
+
+		RenderText(textShader, "46 fps", 20.0f, 20.0f, 0.4f, glm::vec3(1.0f, 0.0f, 0.0f));
 
 		window.update();
 
@@ -107,4 +198,50 @@ void processMovement(Camera* camera, Window* window) {
 
 		camera->look(xoffset, yoffset);
 	}
+}
+
+void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 colour) {
+	s.use();
+	s.setUniform("textColor", colour);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(vao);
+
+	// Iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = characters[*c];
+
+		GLfloat xpos = x + ch.bearing.x * scale;
+		GLfloat ypos = y - (ch.size.y - ch.bearing.y) * scale;
+
+		GLfloat w = ch.size.x * scale;
+		GLfloat h = ch.size.y * scale;
+		// Update VBO for each character
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
 }
